65010e82b569389e8ce810fa60083296057c6bb7
shriharimane <maneshrihari23@gmail.com>
2026-03-07 00:43:47 +0530

    connected frontend API to render backend

--- frontend/src/actions/menuAction.js
+++ frontend/src/actions/menuAction.js
@@ -9,20 +9,21 @@ export const getMenus = (id) => async (dispatch) => {
   try {
     dispatch({ type: GET_MENU_REQUEST });
 
-    const response = await axios.get(
+    const { data } = await axios.get(
       `https://orderitt.onrender.com/api/v1/eats/stores/${id}/menus`
     );
 
-    const menus = response?.data?.data?.[0]?.menu || [];
+    const menuItems = data?.data?.[0]?.menu?.items || [];
 
     dispatch({
       type: GET_MENU_SUCCESS,
-      payload: menus,
+      payload: menuItems,
     });
+
   } catch (error) {
     dispatch({
       type: GET_MENU_FAIL,
-      payload: error.message,
+      payload: error.response?.data?.message || error.message,
     });
   }
 };
